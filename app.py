e5e6430821d21681c6e68095a96be9c7acaa3998
AnFlGue <mokshaflores@icloud.com>
2024-08-20 15:45:27 +0200

    02 Commit

--- app.py
+++ app.py
@@ -1,5 +1,6 @@
-from flask import Flask
+from flask import Flask, render_template, request, redirect, url_for
 from datamanager.sqlite_data_manager import SQLiteDataManager
+from models import User, Movie
 
 app = Flask(__name__)
 data_manager = SQLiteDataManager('moviwebapp.db')  # Use the appropriate path to your Database
@@ -11,7 +12,52 @@ def hello_world():
 @app.route('/users')
 def list_users():
     users = data_manager.get_all_users()
-    return str(users)  # Temporarily returning users as a string
+    return render_template('users.html', users=users)
+
+@app.route('/users/<int:user_id>')
+def user_movies(user_id):
+    user = data_manager.get_user(user_id)
+    movies = data_manager.get_user_movies(user_id)
+    return render_template('user_movies.html', user=user, movies=movies)
+
+@app.route('/add_user', methods=['GET', 'POST'])
+def add_user():
+    if request.method == 'POST':
+        name = request.form['name']
+        new_user = User(user_id=None, name=name)
+        data_manager.add_user(new_user)
+        return redirect(url_for('list_users'))
+    return render_template('add_user.html')
+
+@app.route('/users/<int:user_id>/add_movie', methods=['GET', 'POST'])
+def add_movie(user_id):
+    if request.method == 'POST':
+        name = request.form['name']
+        director = request.form['director']
+        year = request.form['year']
+        rating = request.form['rating']
+        new_movie = Movie(movie_id=None, name=name, director=director, year=year, rating=rating)
+        data_manager.add_movie(user_id, new_movie)
+        return redirect(url_for('user_movies', user_id=user_id))
+    return render_template('add_movie.html', user_id=user_id)
+
+@app.route('/users/<int:user_id>/update_movie/<int:movie_id>', methods=['GET', 'POST'])
+def update_movie(user_id, movie_id):
+    if request.method == 'POST':
+        name = request.form['name']
+        director = request.form['director']
+        year = request.form['year']
+        rating = request.form['rating']
+        updated_movie = Movie(movie_id=movie_id, name=name, director=director, year=year, rating=rating)
+        data_manager.update_movie(user_id, movie_id, updated_movie)
+        return redirect(url_for('user_movies', user_id=user_id))
+    movie = data_manager.get_movie(user_id, movie_id)
+    return render_template('update_movie.html', user_id=user_id, movie=movie)
+
+@app.route('/users/<int:user_id>/delete_movie/<int:movie_id>')
+def delete_movie(user_id, movie_id):
+    data_manager.delete_movie(user_id, movie_id)
+    return redirect(url_for('user_movies', user_id=user_id))
 
 if __name__ == '__main__':
     app.run()
