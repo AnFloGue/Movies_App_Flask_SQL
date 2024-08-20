b5c37cf8a36825c372d8a6e30a30f2a073aacfb6
AnFlGue <mokshaflores@icloud.com>
2024-08-20 15:35:59 +0200

    01 Commit

--- app.py
+++ app.py
@@ -1,12 +1,17 @@
 from flask import Flask
+from datamanager.sqlite_data_manager import SQLiteDataManager
 
 app = Flask(__name__)
-
+data_manager = SQLiteDataManager('moviwebapp.db')  # Use the appropriate path to your Database
 
 @app.route('/')
-def hello_world():  # put application's code here
-    return 'Hello World!'
+def hello_world():
+    return "Welcome to MovieWeb App!"
 
+@app.route('/users')
+def list_users():
+    users = data_manager.get_all_users()
+    return str(users)  # Temporarily returning users as a string
 
 if __name__ == '__main__':
     app.run()
